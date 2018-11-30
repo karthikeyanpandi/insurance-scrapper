89be0cd2f3108caadbcf293466c4d8a5e2d19030
karthikeyanpandi <karthikeyan.p@techmango.net>
2018-11-30 13:22:29 +0530

    Claim number, Upload form added and card view updated

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -35,21 +35,25 @@ $(document).ready(function() {
 
     $("#searchclaim").submit(function(event){
     event.preventDefault(); //prevent default action 
-    showLoader();
-    
-    var post_url = $(this).attr("action"); //get form action url
-    var request_method = $(this).attr("method"); //get form GET/POST method
-    var form_data = $(this).serialize(); //Encode form elements for submission
-    
-    $.ajax({
-    url : post_url,
-    type: request_method,
-    data : form_data
-    }).done(function(response){ 
-        setTimeout(function(){
+    if ($("#searchclaim").valid()) {
+        showLoader();
+        var post_url = $(this).attr("action"); //get form action url
+        var request_method = $(this).attr("method"); //get form GET/POST method
+        var form_data = $(this).serialize(); //Encode form elements for submission
+        console.log(post_url);
+        $.ajax({
+        url : post_url,
+        type: request_method,
+        data : form_data,
+      
+         success: function(){
             hideLoader();
-        },1500);
-    });
+          },
+          error: function(){
+            hideLoader();
+          }
+        });
+    }
     });
 
 } );
